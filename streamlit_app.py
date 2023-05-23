3bd8a2f488bcfeeb63c676e13061a73627e8a825
fkhazami <134418308+fkhazami@users.noreply.github.com>
2023-05-23 12:18:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,12 @@ import streamlit
 
 streamlit.title('my parents new healthy diet')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+
+
 
