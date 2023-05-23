a366b0177777fcdaf29daf1392c91cc4a9be13ba
fkhazami <134418308+fkhazami@users.noreply.github.com>
2023-05-23 12:22:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header(' Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑 Avocado Toast)
+streamlit.text('🥑 Avocado Toast')
 
 
 
